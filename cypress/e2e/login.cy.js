6a841bf39542f092188759e5b165272fd8d97205
cliente <cliente@MacBook-Pro-de-alugacom.local>
2025-01-22 20:35:38 -0300

    Testes centro de ajuda e erros

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -4,9 +4,21 @@ describe('login', () => {
   beforeEach(() => {
     cy.visit('/');
   });
-  it('should redirect to the logged in page successfully', () => {
-    cy.fillInLoginInformation(Cypress.env('cpf'))
-    cy.get('.text-header').should('be.visible')
-    .and('have.text', 'teste')
+//   it('should redirect to the logged in page successfully', () => {
+//     cy.fillInLoginInformation(Cypress.env('validCpf'))
+//     cy.get('.text-header').should('be.visible')
+//     .and('have.text', 'teste')
+//   })
+  it('should validate invalid cpf', () => {
+    cy.fillInLoginInformation(Cypress.env('invalidCpf'))
+    cy.get('#mat-mdc-error-0').should('be.visible')
+    .and('have.text', '*CPF Inválido')
+  })
+  it('should validate help center link successfully', () => {
+    cy.get('.text-primary-700').should('be.visible').click()
+    cy.origin('https://picpay.com/canais-de-atendimento', () => {
+        cy.get('#onetrust-reject-all-handler').should('be.visible').click()
+        cy.get('hgroup > .sc-p448vw-0').should('be.visible').and('have.text', 'Canais de atendimento PicPay')
+      })
   })
 })
